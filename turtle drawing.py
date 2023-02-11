98223b7d0cf9baeed550d73dbc48fd5eeae6386b
PythoniCoder <119647587+PythonistRajat@users.noreply.github.com>
2023-02-11 20:52:33 +0530

    Drawing heart

---
+++ turtle drawing.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+begin_fill()
+color('red','pink')
+left(50)
+forward(100)
+circle(40,180)
+left(260)
+circle(40,180)
+fd(100)
+end_fill()
+
+up()
+setpos(-50,95)
+write('Hey Ms. Hottie\nLove from RAJAT', font = ("Verdana", 10 , "bold"))
+
+done()
