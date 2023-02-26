9ee52d77c6c6421e4cc705dac09bccae9c0bf5d8
PythoniCoder <119647587+PythonistRajat@users.noreply.github.com>
2023-02-26 21:06:51 +0530

    Add files via upload
    This example tells how threading works in Python

---
+++ multithreading in python.py
@@ -0,0 +1,35 @@
+# Multithreading in Python
+
+from threading import Thread
+from time import sleep
+
+class Hi(Thread):
+    def run(self):  # sourcery skip: for-index-underscore
+        for i in range(5):
+            print('hi')
+            sleep(0.25)
+
+class Hello(Thread):
+    def run(self):  # sourcery skip: for-index-underscore
+        for i in range(5):
+            print('hello')
+            sleep(0.25)
+
+t1 = Hi()
+t2 = Hello()
+
+# This execution goes by main thread
+# t1.run()
+# print('Rajat')
+# t2.run()
+# print('Dahiya')
+
+t1.start()
+sleep(0.2)
+t2.start()
+
+# Ask main thread to complete t1 & t2 threads
+t1.join()
+t2.join()
+
+print('Bye')
